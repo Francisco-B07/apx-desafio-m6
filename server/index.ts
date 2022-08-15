1b22bc470e2720fa943808dd39cf089258adac32
Francisco Burgoa <franciscojburgoa@gmail.com>
2022-08-14 23:58:11 -0300

    env vars

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,17 @@
 import * as express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
-import "dotenv/config";
 
 console.log(process.env.NODE_ENV);
 
 app.get("/hola", (req, res) => {
   res.json({
     message: "Hola soy el servidor",
+  });
+});
+app.get("/env", (req, res) => {
+  res.json({
+    environment: process.env.NODE_ENV,
   });
 });
 
