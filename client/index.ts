d338b88e29e4b6ef4a2827361124ddd20b9fddf7
Francisco Burgoa <franciscojburgoa@gmail.com>
2022-10-04 20:07:28 -0300

    los dos jugadores ven la elección del rival

--- client/index.ts
+++ client/index.ts
@@ -8,7 +8,6 @@ import { initPiedra } from "./components/piedra";
 import { initPapel } from "./components/papel";
 import { initPiedraPapelTijera } from "./components/piedra-papel-tijera";
 import { initEstrella } from "./components/estrella";
-import { initNada } from "./components/nada";
 
 // IMPORT DE PAGES
 import "./pages/welcome/index";
@@ -33,7 +32,6 @@ import { Router } from "express";
   initTijera();
   initPiedra();
   initPapel();
-  initNada();
   initPiedraPapelTijera();
   const root = document.querySelector(".root")!;
   // initRouter(root);
